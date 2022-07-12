3f32ed106bb97481ec7e12995633a2b0105c78e7
quochoa304 <109152800+quochoa304@users.noreply.github.com>
2022-07-12 21:59:33 +0700

    Update SongController.php
    them bo dieu kien filler

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -17,7 +17,47 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class SongController extends AbstractController
 {
+/**
+     * @Route("/", name="app_song_index", methods={"GET"})
+     */
+    public function index(SongRepository $songRepository,  Request $request): Response
+    {
+        // 1. Obtain doctrine manager
+        $em = $this->getDoctrine()->getManager();
 
+        // 2. Setup repository of some entity
+        $repoArticles = $em->getRepository(Song::class);
+
+        // 3. Query how many rows are there in the Articles table
+        $totalArticles = $repoArticles->createQueryBuilder('a')
+            // Filter by some parameter if you want
+            // ->where('a.published = 1')
+            ->select('count(a.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        // 4. Return a number as response
+        // e.g 972
+
+        $selectedAname = $request->query->get('Aname');
+        $selectedGenre = $request->query->get('Category');
+        $expressionBuilder = Criteria::expr();
+        $criteria = new Criteria();
+        if (!is_null($selectedGenre)) {
+            $criteria->andWhere($expressionBuilder->eq('Category', $selectedGenre));
+        }
+        if (!is_null($selectedAname)) {
+            $criteria->andWhere($expressionBuilder->eq('Aname', $selectedAname));
+        }
+        $filteredList = $songRepository->matching($criteria);
+
+        return $this->render('song/index.html.twig', [
+            'total' => ($totalArticles),
+            'songs' => $filteredList,
+            'selectedCat' => $selectedGenre ?: 'edm',
+            'selectedAname' => $selectedAname ?: 'Calvin Harris',
+        ]);
+    }
     /**
      * @Route("/new", name="app_song_new", methods={"GET", "POST"})
      */
